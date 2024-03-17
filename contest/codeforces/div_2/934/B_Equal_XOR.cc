1a8785a02b3284479b32106d87baf3f61827a335
Adam-Al-Rahman <quodscientialegis@gmail.com>
2024-03-18 00:58:22 +0530

    update: codeforce contest

--- contest/codeforces/div_2/934/B_Equal_XOR.cc
+++ contest/codeforces/div_2/934/B_Equal_XOR.cc
@@ -1,1 +1,91 @@
-zsh : 1 : command not found : equal
+/*
+ * @copyright
+ * Copyright 2024 Adam-Al-Rahman
+ *
+ * @question: Equal Xor
+ * @brief:
+ * @source: https://codeforces.com/contest/1944/problem/B
+ * @tags: ["topics/constructive_algorithms", "algorithms/", "methods/"]
+ * @status: solved
+ */
+
+// if we don't know n value @compile-time
+// std::vector<std::uint16_t> positions_array[n + 1];
+// or use std::vector<std::vector<std::uint16_t>> positions(n + 1);
+
+#include <cstdint>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+void equal_xor(const std::vector<std::uint16_t> &a, std::uint16_t n,
+               std::uint16_t k) {
+  std::unordered_map<std::uint16_t, std::vector<std::uint16_t>> positions;
+
+  // element: {all indices where it occurred}
+  for (std::int32_t i = 0; i < (2 * n); ++i) {
+    positions[a[i]].push_back(i);
+  }
+
+  std::vector<std::uint16_t> llist;
+  std::vector<std::uint16_t> rlist;
+
+  // add same element in same range in llist and rlist
+  // because, x ^ x = 0
+  // why 1 to n to search, because this way each half part of a[] will be
+  // inserted by sorted in llist and rlist
+  for (std::int32_t i = 1; i <= n; ++i) {
+    if (positions[i].front() < n && positions[i].back() < n &&
+        llist.size() < (2 * k)) {
+      llist.push_back(i);
+      llist.push_back(i);
+    } else if (positions[i].front() >= n && positions[i].back() >= n &&
+               rlist.size() < (2 * k)) {
+      rlist.push_back(i);
+      rlist.push_back(i);
+    }
+  }
+
+  for (std::int32_t i = 1; i <= n; ++i) {
+    // if same elements in same range in llist or rlist
+    // skip because we add it before in above loop
+    if ((positions[i].front() < n && positions[i].back() < n) ||
+        (positions[i].front() >= n && positions[i].back() >= n)) {
+      continue;
+    }
+
+    // add the remaining to cover the llist and rlist size
+    if (llist.size() < (2 * k)) {
+      llist.push_back(i);
+      rlist.push_back(i);
+    }
+  }
+
+  // print llist elements
+  for (std::uint16_t &element : llist) std::cout << element << ' ';
+  std::cout << '\n';
+
+  // print rlist elements
+  for (std::uint16_t &element : rlist) std::cout << element << ' ';
+  std::cout << std::flush;
+}
+
+int main() {
+  std::uint16_t tests;
+  std::cin >> tests;
+
+  while (tests--) {
+    std::uint16_t n;
+    std::cin >> n;
+
+    std::uint16_t k;
+    std::cin >> k;
+
+    std::vector<std::uint16_t> a(2 * n);
+    for (std::uint16_t &element : a) {
+      std::cin >> element;
+    }
+
+    equal_xor(a, n, k);
+  }
+}
--- test.cc
+++ test.cc
@@ -1,45 +1,63 @@
-/**
- *    Copyright tourist 2024
- *    created: 15.03.2024 10:39:06
- **/
-
 #include <bits/stdc++.h>
+#define pb push_back
+#define F first
+#define S second
+#define all(v) v.begin(), v.end()
+#define Gondozu                \
+  ios::sync_with_stdio(false); \
+  cout.tie(NULL);              \
+  cin.tie(NULL);
 
 using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vpi = vector<pair<int, int>>;
+using vvi = vector<vector<int>>;
 
-#ifdef LOCAL
-#include "algo/debug.h"
-#else
-#define debug(...) 42
+const int OO = 1e9 + 5;
+const int N = 2e5 + 5;
+
+void TC() {
+  int n, k;
+  cin >> n >> k;
+  int a[n * 2];
+  vi pos[n + 1];
+  for (int i = 0; i < n * 2; ++i) {
+    cin >> a[i];
+    pos[a[i]].pb(i);
+  }
+
+  vector<int> l, r;
+  for (int i = 1; i <= n; ++i) {
+    if (pos[i].front() < n && pos[i].back() < n && l.size() < 2 * k) {
+      l.pb(i), l.pb(i);
+    } else if (pos[i].front() >= n && pos[i].back() >= n && r.size() < 2 * k)
+      r.pb(i), r.pb(i);
+  }
+
+  for (int i = 1; i <= n; ++i) {
+    if ((pos[i].front() < n && pos[i].back() < n) ||
+        (pos[i].front() >= n && pos[i].back() >= n))
+      continue;
+    if (l.size() < 2 * k) l.pb(i), r.pb(i);
+  }
+
+  for (int i : l) cout << i << ' ';
+  cout << '\n';
+  for (int i : r) cout << i << ' ';
+}
+
+int32_t main() {
+#ifndef ONLINE_JUDGE
+  freopen("input.in", "r", stdin);
+  freopen("output.out", "w", stdout);
 #endif
-
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  int tt;
-  cin >> tt;
-  while (tt--) {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-      cin >> a[i];
-    }
-    bool fail = false;
-    int p = 0;
-    for (int x : a) {
-      if (x >= 10 && x / 10 >= p && x % 10 >= x / 10) {
-        p = x % 10;
-      } else {
-        if (x >= p) {
-          p = x;
-        } else {
-          fail = true;
-          break;
-        }
-      }
-    }
-    cout << (fail ? "NO" : "YES") << '\n';
+  Gondozu int t = 1;
+  cin >> t;
+  while (t--) {
+    TC();
+    cout << '\n';
   }
   return 0;
 }
