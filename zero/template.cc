b635726045b56cf61a5af4b3eec62502f3a6eb1d
Adam-Al-Rahman <quodscientialegis@gmail.com>
2024-12-29 03:54:03 +0530

    update: template

--- zero/template.cc
+++ zero/template.cc
@@ -1,10 +1,7 @@
 
-// HEADERS [Required]
+// HEADERS
 #ifndef ONLINE_JUDGE
-#include <sys/resource.h>  // For getrusage
-
 #include <cstdio>  // freopen
-#include <ctime>   // std::clock
 
 #include "cpp-dump/cpp-dump.hpp"  // cpp_dump (debug)
 #endif                            // ONLINE_JUDGE
@@ -12,17 +9,13 @@
 #include <cstdint>  // std::int32_t, std::int16_t, std::int64_t
 #include <ios>      // std::ios_base
 #include <iostream>
-#include <tuple>
 
 // ALIAS | STRUCT | CLASS | HELPER FUNCTIONS
-namespace px {
-using node = std::tuple<std::int64_t, std::int64_t>;  // NOTE: {high priority, low priority }
-}  // namespace px
+namespace px {}
 
 // PROBLEM KEYPOINTS
 void solution() {}
 
-// MAIN
 int main() {
   std::ios_base::sync_with_stdio(0);
   std::cin.tie(0);
@@ -33,21 +26,7 @@ int main() {
   (void)freopen("./zero/output.txt", "w", stdout);
 #endif  // ONLINE_JUDGE
 
-#ifndef ONLINE_JUDGE
-  std::clock_t start_time = std::clock();
-#endif  // ONLINE_JUDGE
-
   std::uint32_t tests = 1;
-  std::cin >> tests;  // overwrite
+  std::cin >> tests;  // OVERWRITE
   while (tests--) solution();
-
-#ifndef ONLINE_JUDGE
-  std::clock_t end_time = std::clock();
-  std::cerr << "Empirical Run Time (SEC): " << (static_cast<double>(end_time - start_time) / CLOCKS_PER_SEC)
-            << std::endl;
-
-  struct rusage usage;
-  getrusage(RUSAGE_SELF, &usage);
-  std::cerr << "Empirical Memory Usage (MB): " << (usage.ru_maxrss / 1024.0) << std::endl;
-#endif  // ONLINE_JUDGE
 }
