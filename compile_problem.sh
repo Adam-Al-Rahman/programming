06a281140b927930819e01f45f126d5d06a6b1ec
Adam-Al-Rahman <quodscientialegis@gmail.com>
2025-08-13 08:29:56 +0000

    update: compile.sh

--- compile_problem.sh
+++ compile_problem.sh
@@ -11,10 +11,10 @@ clang++ -std=c++20 "$SOURCE_FILE" -o "$OUTPUT_FILE"
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
-	echo -e "Compilation \033[0;32msuccessful\033[0m."
-	echo -e "Executing: \033[0;32m$OUTPUT_FILE\033[0m"
+	echo -e "Compilation successful"
+	echo -e "Executing: $OUTPUT_FILE"
 	echo "" # Adds a new line
 	./$OUTPUT_FILE
 else
-	echo -e "Compilation \033[0;31mfailed\033[0m."
+	echo -e "Compilation: Failed"
 fi
