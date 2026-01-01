e9c98997646fdde915cc570581cd126a3affe462
Adam-Al-Rahman <adam.al.rahman.dev@gmail.com>
2026-01-01 08:15:11 +0000

    add: rust lang support and skeleton

--- rst_problem.sh
+++ rst_problem.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-template_file="template.cc"
-output_file="problem.cc"
 
-# Overwrite contents without replacing the file (inode-safe for Neovim)
+lang="$1"
+
+case "$lang" in
+  rs)
+    template_file="skeleton.rs"
+    output_file="problem.rs"
+    ;;
+  cc)
+    template_file="skeleton.cc"
+    output_file="problem.cc"
+    ;;
+  *)
+    echo "Usage: $0 {rs|cc}"
+    exit 1
+    ;;
+esac
+
+# Inode-safe overwrite (Neovim-friendly)
 cat "$template_file" > "$output_file"
---
+++ skeleton.rs
@@ -0,0 +1,54 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, BufWriter, Write};
+
+fn solve<R: BufRead, W: Write>(scan: &mut Scanner<R>, out: &mut W) {}
+
+fn main() {
+    let input: Box<dyn BufRead> = if cfg!(debug_assertions) {
+        Box::new(BufReader::new(
+            File::open("input.txt").expect("input.txt not found"),
+        ))
+    } else {
+        Box::new(BufReader::new(io::stdin()))
+    };
+
+    let mut output: Box<dyn Write> = if cfg!(debug_assertions) {
+        Box::new(BufWriter::new(
+            File::create("output.txt").expect("cannot create output.txt"),
+        ))
+    } else {
+        Box::new(BufWriter::new(io::stdout()))
+    };
+
+    let mut scan = Scanner::new(input);
+
+    let tests: usize = scan.next();
+    for _ in 0..tests {
+        solve(&mut scan, &mut output);
+    }
+}
+
+struct Scanner<R> {
+    reader: R,
+    buffer: Vec<String>,
+}
+
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buffer: Vec::new(),
+        }
+    }
+
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Parse error");
+            }
+            let mut input = String::new();
+            self.reader.read_line(&mut input).unwrap();
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+}
